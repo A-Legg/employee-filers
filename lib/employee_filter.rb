2cd0b2dcbb24ee341d922dbfd3cd2d3d8dc0b883
Adam Legg <adam.legg@gmail.com>
2014-07-14 07:43:52 -0600

    passing tests

--- lib/employee_filter.rb
+++ lib/employee_filter.rb
@@ -1,3 +1,35 @@
+
+
+
+# class EmployeeFilter
+#
+#
+#   def new_employees
+#     # @employees = []
+#     #  EMPLYOYEES.each do |new|
+#     #    if new[:start_date][0] < 2006 << @employees
+#     #
+#     #    end
+#     #  end
+#     # @employees
+#
+#   end
+#
+# end
+
+
 class EmployeeFilter
+  def initialize(employees)
+    @employees = employees
+  end
+
+  def started_before_2006
+    @employees.select { |employee_hash| employee_hash[:start_date] < Date.new(2006)}
+  end
+
+  def all_with_start_date
+    @employees.collect { |employee_hash| "#{employee_hash[:first_name]} #{employee_hash[:last_name]} (#{employee_hash[:title]}) - #{employee_hash[:start_date].strftime("%-m/%-d/%Y")}"}
+  end
 
 end
+
